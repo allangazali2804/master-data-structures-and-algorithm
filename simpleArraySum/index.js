72c3823b199853aca8fce7244d656c47381e4ed4
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-08-14 17:54:41 +0700

    Add files via upload

---
+++ simpleArraySum/index.js
@@ -0,0 +1,24 @@
+/*
+Given an array of integers, find the sum of its elements.
+
+For example, if the array , , so return .
+
+Function Description
+
+Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+
+ar: an array of integers
+*/
+
+function simpleArraySum(ar) {
+    let num = 0;
+    
+    for(let i=0; i < ar.length; i++){
+        num += ar[i];
+    }
+    
+    return num;
+
+}
