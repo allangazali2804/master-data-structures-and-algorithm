5fcb5ee9fdb4f6a913caf4487f1ea9070350ac8d
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-08-15 14:54:47 +0700

    Update index.js

--- plusMinus/index.js
+++ plusMinus/index.js
@@ -6,8 +6,8 @@ Note: This challenge introduces precision problems. The test cases are scaled to
 
 function plusMinus(arr) {
     let positive = 0;
-    let zero = 0;
     let negative = 0;
+    let zero = 0;
     for(let i=0; i < arr.length; i++){
         if (arr[i] > 0){
             positive++;
