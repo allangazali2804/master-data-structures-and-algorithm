e7b14d9dc638270207c8b29f19eeab822f175821
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-09-24 20:58:19 +0700

    Update index.js

--- 15.7 Who's Buying Lunch/index.js
+++ 15.7 Who's Buying Lunch/index.js
@@ -1,29 +1,9 @@
-/* 
-
-1. Challengenya adalah mengacak nama dari sebuah array sehingga outputnya akan memberikan salah satu nilai dari array. 
-
-*/ 
-
 function whosPaying(names) { 
 
-    // mengetahui jumlah item di dalam sebuah array. 
+    const randomPerson = Math.floor(Math.random() * names.length);  
 
-    const numberOfPeople = names.length; 
-
-    // mengacak angka yang sesuai dengan jumlah item di dalam array 
-
-    const randomPerson = Math.floor(Math.random() * numberOfPeople); 
-
-    // mengakses item di dalam array 
-
-    const personToPay = names[randomPerson]; 
-
-    //memberikan output berupa item dalam array dan sejumlah string dengan keyword return 
-
-    return personToPay + " is going to buy lunch today!"; 
+    return names[randomPerson] + " is going to buy lunch today!"; 
 
 } 
 
 alert(whosPaying(["Allan", "Yuki", "Kaze", "Hi", "Tsuchi", "Mizu", "Kaminari"])); 
-
- 
