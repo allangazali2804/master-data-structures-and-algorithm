6e37f85d4089240a588bef56899999178e0c96de
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-10-28 21:08:10 +0700

    Update index.js

--- leapYear/index.js
+++ leapYear/index.js
@@ -19,4 +19,3 @@ alert(isLeap(year));
 
 // Challenge di atas adalah membuat sebuah pengujian apakah tahun yang diinput merupakan tahun kabisat atau bukan.
 // Ada banyak kondisi yang harus dipenuhi, sehingga pembuatan flowchart akan sangat membantu.
-// Setelah flowchart telah selesai dibuat, maka harus diterjemahkan ke dalam kode.
