4fe4cfbc5186f8cede881c917712525704bb313a
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-10-29 19:45:39 +0700

    Update index.js

--- 018maximumWords/index.js
+++ 018maximumWords/index.js
@@ -2,7 +2,7 @@ var mostWordsFound = function(sentences) {
     let max = 0;
     let temp = 0;
     for (let i = 0; i < sentences.length; i++) {
-        temp = sentences[i].split(" ").length;
+        temp = sentences[i].split(" ").length; //array method .split()
         if (temp > max) {
             max = temp;
         }
